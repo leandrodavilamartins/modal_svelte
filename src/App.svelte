602234f8d6257bab13baf4b9d0839835d45e4da5
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2022-04-10 16:24:33 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,71 @@
+<script>
+  import Modal from "./Modal.svelte";
+
+  let todos = [];
+  let item;
+  let quant;
+
+  function add() {
+    todos = [...todos, ""];
+  }
+
+  let formulaModalOpen = false;
+  let misturaModalOpen = false;
+
+  $: console.log(item);
+  $: console.log(quant);
+</script>
+
+<main>
+  <button class="btn btn-warning" on:click={() => (formulaModalOpen = true)}
+    >Fórmula</button
+  >
+  <button class="btn btn-warning" on:click={() => (misturaModalOpen = true)}
+    >Mistura</button
+  >
+  <Modal
+    open={formulaModalOpen}
+    onClosed={() => (formulaModalOpen = false)}
+    title="Fórmula"
+  >
+    {#each todos as todo, index}
+      <div class="form-line">
+        <input type="text" placeholder="Matéria-prima" bind:value={item} />
+        <input type="number" placeholder="Quantidade" bind:value={quant} />
+        <button class="btn btn-danger">X</button>
+      </div>
+    {/each}
+    <div>
+      <br />
+      <button class="btn btn-warning" on:click={add}>Adicionar</button>
+    </div>
+  </Modal>
+
+  <Modal
+    open={misturaModalOpen}
+    onClosed={() => (misturaModalOpen = false)}
+    title="Mistura"
+  />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+  .form-line {
+    display: block;
+  }
+  input {
+    background-color: #212529;
+    color: whitesmoke;
+  }
+</style>
---
+++ src/Modal.svelte
@@ -0,0 +1,72 @@
+<script>
+  export let open = false;
+  export let showBackdrop = true;
+  export let onClosed; // this is a function exported to the parent as a prop !!!
+  export let title;
+
+  const modalClose = () => {
+    open = false;
+    if (onClosed) {
+      // when the modal is closed the function is called !!!
+      onClosed();
+    }
+  };
+</script>
+
+{#if open}
+  <div
+    class="modal"
+    id="sampleModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="sampleModalLabel"
+    aria-hidden={false}
+  >
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="sampleModalLabel">{title}</h5>
+          <button
+            type="button"
+            class="close btn btn-dark"
+            data-dismiss="modal"
+            aria-label="Close"
+            on:click={modalClose}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body"><slot /></div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-dismiss="modal"
+            on:click={modalClose}>Cancelar</button
+          >
+          <button type="button" class="btn btn-primary">Salvar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  {#if showBackdrop}
+    <div class="modal-backdrop show" />
+  {/if}
+{/if}
+
+<style>
+  .modal {
+    display: block;
+  }
+  .modal-body {
+    background-color: #212529;
+  }
+  .modal-header {
+    background-color: #212529;
+    color: whitesmoke;
+  }
+  .modal-footer {
+    background-color: #212529;
+    color: whitesmoke;
+  }
+</style>
